76814dc156aaaa8d5bfb14f17562569a787d58c4
975851497 <7767701+z975851497@user.noreply.gitee.com>
2026-01-07 00:16:46 +0800

    feat:重构代码，提取监听事件和绘制方法

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,18 +25,23 @@ class AlienInvasion:
     def run_game(self):
         """开始游戏的主循环"""
         while True:
-            # 监听键盘和鼠标
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # 每次循环时都重绘屏幕
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
             self.clock.tick(60)
+
+    def _check_events(self):
+        # 监听键盘和鼠标
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        # 每次循环时都重绘屏幕
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # 创建游戏实例并且运行
